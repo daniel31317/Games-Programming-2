682d5aaea2875801b5e34e52ecb9c0fa43ff115c
Daniel <157810096+daniel31317@users.noreply.github.com>
2026-02-09 15:19:29 +0000

    added second triangle

--- Lab1/MainGame.cpp
+++ Lab1/MainGame.cpp
@@ -24,6 +24,8 @@ void MainGame::initSystems()
 
 void MainGame::gameLoop()
 {
+	
+
 	while (_gameState != GameState::EXIT)
 	{
 		processInput();
@@ -54,12 +56,19 @@ void MainGame::drawGame()
 	Vertex vertices[] = { Vertex(glm::vec3(-0.5, -0.5, 0), glm::vec2(0.0, 0.0)),
 	Vertex(glm::vec3(0, 0.5, 0), glm::vec2(0.5, 1.0)),
 	Vertex(glm::vec3(0.5, -0.5, 0), glm::vec2(1.0, 0.0)) };
+	
+	
 
-	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0])); //size calcuated by number of bytes of an array / no bytes of one element
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
+	Mesh mesh1(vertices, sizeof(vertices) / sizeof(vertices[0]));
 
 	Shader shader("..\\res\\shader"); //new shader
 
 	Texture texture("..\\res\\bricks.jpg"); //load texture
+	Texture texture1("..\\res\\Water.jpg"); //load texture
+
 
 
 	Transform transform;
@@ -73,6 +82,14 @@ void MainGame::drawGame()
 	mesh.Draw();
 
 	counter += 0.01f;
+
+	transform.SetPos(glm::vec3(-sinf(counter), 0.0, 0.0));
+	transform.SetRot(glm::vec3(0.0, 0.0, counter * 5));
+	transform.SetScale(glm::vec3(-sinf(counter), -sinf(counter), -sinf(counter)));
+
+	shader.Update(transform);
+	texture1.Bind(0);
+	mesh.Draw();
 
 	glEnableClientState(GL_COLOR_ARRAY);
 
