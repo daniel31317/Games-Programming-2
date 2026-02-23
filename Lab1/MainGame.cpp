75019205383d9a0d3291808fbdb1190f22e79dc9
Daniel <danielmj003@gmail.com>
2026-02-23 15:08:10 +0000

    added toon shader to make monkey evil with light

--- Lab1/MainGame.cpp
+++ Lab1/MainGame.cpp
@@ -26,7 +26,7 @@ void MainGame::initSystems()
 {
 	_gameDisplay.initDisplay();
 	mesh.loadModel("..\\res\\monkey3.obj");
-	shader = std::make_unique<Shader>("..\\res\\fog"); //new shader
+	shader = std::make_unique<Shader>("..\\res\\toon"); //new shader
 	texture = std::make_unique<Texture>("..\\res\\water.jpg"); //load texture
 	myCamera.initCamera(glm::vec3(0, 0, -5), 70.0f, (float)_gameDisplay.getWidth() / _gameDisplay.getHeight(), 0.01f, 1000.0f);
 }
---
+++ res/toon.frag
@@ -0,0 +1,18 @@
+#version 400
+
+in vec3 normal;
+
+
+void main()
+{
+	vec3 lightDir = vec3(0.0, -1.0, 0.5);
+
+	float brightness = 1.0f;
+
+    float intensity = dot(normal, lightDir) * brightness;
+
+    vec3 baseColor = vec3(255, 0, 212) / 255.0;
+
+    gl_FragColor = vec4(baseColor * intensity, 1.0);
+}
+
