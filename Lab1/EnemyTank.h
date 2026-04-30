a3b397cc61f7a76a413b718530f2501851f3bcac
Daniel <crimpygymnast24@gmail.com>
2026-04-30 01:59:42 +0100

    added co-ordinates for street

--- Lab1/EnemyTank.h
+++ Lab1/EnemyTank.h
@@ -3,6 +3,7 @@
 #include "MeshManager.h"
 #include "ShaderManager.h"
 #include "TextureManager.h"
+#include <array>
 
 struct EnemyTank
 {
@@ -13,7 +14,12 @@ public:
 
 	EnemyTank(ShaderManager& shaderManager, TextureManager& textureManager, MeshManager& meshManager, Camera* mainCamera) : m_tank(shaderManager, textureManager, meshManager, mainCamera, false)
 	{
-		m_tank.SetPosition(glm::vec3(2.0, -0.5, -1.5));
+
+		int indexX = 4;
+		int indexZ = 5;
+
+
+		m_tank.SetPosition(glm::vec3(movementGrid[indexX][indexZ].x, -0.5, -movementGrid[indexX][indexZ].y));
 	}
 
 
@@ -30,5 +36,20 @@ public:
 private:
 
 	Tank m_tank;
+
+	//really cursed layout but hey
+	//bottom left (x,z)	(0,0)																																																//top left				
+	const std::array<std::array<glm::vec2, 8>, 9> movementGrid = { {
+	{ glm::vec2(99.5f, 101.5f),  glm::vec2(99.5f, 78.5f),  glm::vec2(99.5f, 55.5f),  glm::vec2(99.5f, 33.0f),  glm::vec2(99.5f, 10.0f),  glm::vec2(99.5f, -13.5f),  glm::vec2(99.5f, -35.5f),  glm::vec2(99.5f, -57.0f)  },
+	{ glm::vec2(75.0f, 101.5f),  glm::vec2(75.0f, 78.5f),  glm::vec2(75.0f, 55.5f),  glm::vec2(75.0f, 33.0f),  glm::vec2(75.0f, 10.0f),  glm::vec2(75.0f, -13.5f),  glm::vec2(75.0f, -35.5f),  glm::vec2(75.0f, -57.0f)  },
+	{ glm::vec2(50.0f, 101.5f),  glm::vec2(50.0f, 78.5f),  glm::vec2(50.0f, 55.5f),  glm::vec2(50.0f, 33.0f),  glm::vec2(50.0f, 10.0f),  glm::vec2(50.0f, -13.5f),  glm::vec2(50.0f, -35.5f),  glm::vec2(50.0f, -57.0f)  },
+	{ glm::vec2(25.0f, 101.5f),  glm::vec2(25.0f, 78.5f),  glm::vec2(25.0f, 55.5f),  glm::vec2(25.0f, 33.0f),  glm::vec2(25.0f, 10.0f),  glm::vec2(25.0f, -13.5f),  glm::vec2(25.0f, -35.5f),  glm::vec2(25.0f, -57.0f)  },
+	{ glm::vec2(-0.5f, 101.5f),  glm::vec2(-0.5f, 78.5f),  glm::vec2(-0.5f, 55.5f),  glm::vec2(-0.5f, 33.0f),  glm::vec2(-0.5f, 10.0f),  glm::vec2(-0.5f, -13.5f),  glm::vec2(-0.5f, -35.5f),  glm::vec2(-0.5f, -57.0f)  },
+	{ glm::vec2(-25.0f, 101.5f), glm::vec2(-25.0f, 78.5f), glm::vec2(-25.0f, 55.5f), glm::vec2(-25.0f, 33.0f), glm::vec2(-25.0f, 10.0f), glm::vec2(-25.0f, -13.5f), glm::vec2(-25.0f, -35.5f), glm::vec2(-25.0f, -57.0f) },
+	{ glm::vec2(-49.0f, 101.5f), glm::vec2(-49.0f, 78.5f), glm::vec2(-49.0f, 55.5f), glm::vec2(-49.0f, 33.0f), glm::vec2(-49.0f, 10.0f), glm::vec2(-49.0f, -13.5f), glm::vec2(-49.0f, -35.5f), glm::vec2(-49.0f, -57.0f) },
+	{ glm::vec2(-76.0f, 101.5f), glm::vec2(-76.0f, 78.5f), glm::vec2(-76.0f, 55.5f), glm::vec2(-76.0f, 33.0f), glm::vec2(-76.0f, 10.0f), glm::vec2(-76.0f, -13.5f), glm::vec2(-76.0f, -35.5f), glm::vec2(-76.0f, -57.0f) },
+	{ glm::vec2(-99.0f, 101.5f), glm::vec2(-99.0f, 78.5f), glm::vec2(-99.0f, 55.5f), glm::vec2(-99.0f, 33.0f), glm::vec2(-99.0f, 10.0f), glm::vec2(-99.0f, -13.5f), glm::vec2(-99.0f, -35.5f), glm::vec2(-99.0f, -57.0f) }
+} };
+	//bottom right																																																	//top right
 
 };
