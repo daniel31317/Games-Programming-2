83bacd72d6e241e660ac4309384920f887adaf66
Daniel <crimpygymnast24@gmail.com>
2026-05-01 09:53:16 +0100

    removed uneeded debug msg

--- Lab1/MainGame.cpp
+++ Lab1/MainGame.cpp
@@ -367,9 +367,7 @@ void MainGame::processInput()
 
 		m_tank->UpdateTurretAim(deltaTime, m_targetTurretAngle, m_targetBarrelPitch, isZooming, 
 			glm::distance(*m_tank->GetBody()->GetTransform()->GetPosition(), m_colliderEditor->ShootDetection(*m_tank->GetBarrel()->GetTransform()->GetPosition(), m_tank->GetBarrel()->GetTransform()->GetForward(), true, false)));
-
-
-		std::cout << std::to_string(glm::distance(*m_tank->GetBody()->GetTransform()->GetPosition(), m_colliderEditor->ShootDetection(*m_tank->GetBarrel()->GetTransform()->GetPosition(), m_tank->GetBarrel()->GetTransform()->GetForward(), true, false))) << std::endl;
+		
 
 
 		if (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT))
